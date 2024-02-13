da47eddc93c47560b0fd38e989736c2813bd7263
Alim Weber <alim.weber@outlook.com>
2024-02-13 18:47:02 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,33 @@
+const app = require("localhostjs");
+const express = app.express; 
+require("dotenv").config()
+
+const env = require("./env.json")
+const path = require("path")
+
+app.rest.use(express.static(path.join(__dirname, "static")))
+
+
+let clients = []
+
+app.socket.on("register", (data) => {
+	
+	clients.push(data)
+	
+	console.log(data)
+
+	app.socket.send("registered", true, [data])
+
+})
+
+app.socket.on("message", (data) => {
+
+	let {id, message} = data
+
+	app.socket.send("message", {
+		message, id
+	}, clients.filter(s => s!== id))
+
+})
+
+app.listen(env)
